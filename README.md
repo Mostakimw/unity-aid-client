78fa361e63b2697894ce17efac2194499752f212
Mostakimw <scientist.expert@gmail.com>
2024-05-03 16:37:54 +0600

    initial commit with react router dom basic setup

---
+++ README.md
@@ -0,0 +1,1 @@
+# Unity Aid Website
---
+++ src/routes/routes.tsx
@@ -0,0 +1,21 @@
+import { createBrowserRouter } from "react-router-dom";
+import App from "../App";
+import Login from "../pages/Login/Login";
+import Register from "../pages/Register/Register";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+]);
+
+export default router;
