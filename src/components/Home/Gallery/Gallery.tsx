60861d46a3120447ba7f71c422b4562c5396ea3d
Mostakimw <scientist.expert@gmail.com>
2024-05-05 18:00:03 +0600

    gallery done and responsivness

--- src/components/Home/Gallery/Gallery.tsx
+++ src/components/Home/Gallery/Gallery.tsx
@@ -2,23 +2,59 @@ import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from "react-responsive-carousel";
 import Container from "../../Container/Container";
 import "./Gallery.css";
-import GalleryCard from "./GalleryCard";
+import { GalleryCard, MobileGalleryCard } from "./GalleryCard";
+import { useMediaQuery } from "react-responsive";
+
+const imageUrls = [
+  [
+    "https://img.freepik.com/free-photo/people-stacking-hands-together-park_53876-63293.jpg",
+    "https://img.freepik.com/free-photo/people-stacking-hands-together-park_53876-63293.jpg",
+    "https://img.freepik.com/free-photo/people-stacking-hands-together-park_53876-63293.jpg",
+    "https://img.freepik.com/free-photo/people-stacking-hands-together-park_53876-63293.jpg",
+  ],
+  [
+    "https://img.freepik.com/free-photo/people-stacking-hands-together-park_53876-63293.jpg",
+    "https://img.freepik.com/free-photo/people-stacking-hands-together-park_53876-63293.jpg",
+    "https://img.freepik.com/free-photo/people-stacking-hands-together-park_53876-63293.jpg",
+    "https://img.freepik.com/free-photo/people-stacking-hands-together-park_53876-63293.jpg",
+  ],
+];
+
+const mobileImageUrls = [
+  "https://img.freepik.com/free-photo/people-stacking-hands-together-park_53876-63293.jpg",
+  "https://img.freepik.com/free-photo/people-stacking-hands-together-park_53876-63293.jpg",
+  "https://img.freepik.com/free-photo/people-stacking-hands-together-park_53876-63293.jpg",
+  "https://img.freepik.com/free-photo/people-stacking-hands-together-park_53876-63293.jpg",
+
+  "https://img.freepik.com/free-photo/people-stacking-hands-together-park_53876-63293.jpg",
+  "https://img.freepik.com/free-photo/people-stacking-hands-together-park_53876-63293.jpg",
+  "https://img.freepik.com/free-photo/people-stacking-hands-together-park_53876-63293.jpg",
+  "https://img.freepik.com/free-photo/people-stacking-hands-together-park_53876-63293.jpg",
+];
 
 const Gallery = () => {
+  const isMobile = useMediaQuery({ maxWidth: 768 });
+
   return (
     <Container style={{ marginTop: 112 }}>
       <div className="gallery">
-        <Carousel showThumbs={false} showStatus={false} infiniteLoop>
-          <div>
-            <GalleryCard />
-          </div>
-          <div>
-            <GalleryCard />
-          </div>
-          <div>
-            <GalleryCard />
-          </div>
-        </Carousel>
+        {/* carousel for medium and large devices */}
+        {!isMobile ? (
+          <Carousel showThumbs={false} showStatus={false} infiniteLoop>
+            {imageUrls.map((images, index) => (
+              <GalleryCard key={index} images={images} />
+            ))}
+          </Carousel>
+        ) : (
+          <>
+            {/* carousel for mobile devices */}
+            <Carousel showThumbs={false} showStatus={false} infiniteLoop>
+              {mobileImageUrls.map((image, index) => (
+                <MobileGalleryCard key={index} image={image} />
+              ))}
+            </Carousel>
+          </>
+        )}
       </div>
     </Container>
   );
--- src/components/Home/Gallery/GalleryCard.tsx
+++ src/components/Home/Gallery/GalleryCard.tsx
@@ -1,34 +1,44 @@
-const GalleryCard = () => {
+export const GalleryCard = ({ images }: { images: string[] }) => {
   return (
     <div className="grid grid-cols-1 md:grid-cols-3 gap-6 w-full ">
-          <div>
-            <img
-              src="https://img.freepik.com/free-photo/people-stacking-hands-together-park_53876-63293.jpg"
-              className="w-full h-full object-cover"
-              alt=""
-            />
-          </div>
-          <div className="flex flex-col justify-between">
-            <img
-              src="https://img.freepik.com/free-photo/people-stacking-hands-together-park_53876-63293.jpg"
-              className="w-full h-1/2 object-cover mb-3"
-              alt=""
-            />
-            <img
-              src="https://img.freepik.com/free-photo/people-stacking-hands-together-park_53876-63293.jpg"
-              className="w-full h-1/2 object-cover"
-              alt=""
-            />
-          </div>
-          <div>
-            <img
-              src="https://img.freepik.com/free-photo/people-stacking-hands-together-park_53876-63293.jpg"
-              className="w-full h-full object-cover"
-              alt=""
-            />
-          </div>
-        </div>
+      <div>
+        <img
+          src={images[0]}
+          className="w-full h-full object-cover"
+          alt="Donation Gallery Image"
+        />
+      </div>
+      <div className="flex flex-col justify-between">
+        <img
+          src={images[1]}
+          className="w-full h-1/2 object-cover mb-3"
+          alt="Donation Gallery Image"
+        />
+        <img
+          src={images[2]}
+          className="w-full h-1/2 object-cover"
+          alt="Donation Gallery Image"
+        />
+      </div>
+      <div>
+        <img
+          src={images[3]}
+          className="w-full h-full object-cover"
+          alt="Donation Gallery Image"
+        />
+      </div>
+    </div>
   );
 };
 
-export default GalleryCard;
+export const MobileGalleryCard = ({ image }: { image: string }) => {
+  return (
+    <div>
+      <img
+        src={image}
+        className="w-full h-full object-cover"
+        alt="Donation Gallery Image"
+      />
+    </div>
+  );
+};
