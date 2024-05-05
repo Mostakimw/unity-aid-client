52a3bc6759286801219f2215dfa363094e253bb6
Mostakimw <scientist.expert@gmail.com>
2024-05-05 20:17:34 +0600

    donation responsived

--- src/components/Home/DonationSection/DonationSection.tsx
+++ src/components/Home/DonationSection/DonationSection.tsx
@@ -20,11 +20,18 @@ const DonationSection = () => {
       <div className="mt-16">
         <Row gutter={24}>
           {data.map((item) => (
-            <Col key={item.id} span={8} className=" w-full  gutter-row mb-8">
+            <Col
+              key={item.id}
+              span={24}
+              md={{ span: 12 }}
+              lg={{ span: 8 }}
+              className="w-full  gutter-row mb-8"
+            >
               <Card
                 hoverable
                 cover={
                   <img
+                    title={item.title}
                     alt="example"
                     src={item.image}
                     className="w-full h-96  object-cover bg-[#FEE3BD]"
@@ -32,7 +39,7 @@ const DonationSection = () => {
                 }
               >
                 <div className="flex gap-4 justify-between">
-                  <p className="text-2xl font-bold">{item.title}</p>
+                  <p className="text-2xl font-bold truncate">{item.title}</p>
                   <p className="mt-[5px] ">#{item.category}</p>
                 </div>
                 <div className="flex justify-between items-center mt-4">
